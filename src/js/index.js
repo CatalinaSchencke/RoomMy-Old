11bdf084b76ac334f34ced7196ec2958be98dda9
Gonzalo Bruna <catalina.schencke.p@mail.pucv.cl>
2023-04-21 11:56:58 -0400

    change accordion side text based on accordion item

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,31 @@
 let accordionButtons = document.querySelectorAll(".accordion-button");
+let activeContent = document.querySelector("#activeContent");
 
 accordionButtons.forEach(button => {
+
     button.addEventListener('click', function () {
         let contentId = button.dataset.accordionTarget;
-        accordionButtons.forEach(button => {
-            if (button.dataset.accordionTarget != contentId) {
-                let content = document.querySelector(button.dataset.accordionTarget);
-                content.classList.add("max-h-0");
-                content.classList.remove("max-h-[200px]");
-            }
-        });
+        let sourceContent = button.dataset.accordionSource;
 
-        let content = document.querySelector(contentId);
-        content.classList.toggle("max-h-0");
-        content.classList.toggle("max-h-[200px]");
+        closeOpenAccordions(contentId);        
+
+        let accordionContent = document.querySelector(contentId);
+        let sourceText = document.querySelector(sourceContent).innerHTML;
+        activeContent.innerHTML = sourceText;
+        accordionContent.classList.toggle("max-h-0");
+        accordionContent.classList.toggle("max-h-[200px]");
     });
 });
+
+function closeOpenAccordions(contentId) {
+    accordionButtons.forEach(button => {
+        if (button.dataset.accordionTarget != contentId) {
+            let content = document.querySelector(button.dataset.accordionTarget);
+            content.classList.add("max-h-0");
+            content.classList.remove("max-h-[200px]");
+        }
+    });
+}
 
 // setupAccordion();
 
