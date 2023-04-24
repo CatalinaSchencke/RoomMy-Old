53c8beb342350ed8ffaae08a28fb6d292ddb3c43
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-04-23 21:18:56 -0400

    add custom colors and apply them

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-blue": "#5DA8BD",
+        "darken-blue": "#4A758E",
+        "vivid-blue":"#2596be",
+      },
+    },
     screens: {
       'xs': '400px',
       ...defaultTheme.screens,
